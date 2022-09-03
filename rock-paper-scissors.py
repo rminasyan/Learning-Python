62c4e8fd9cac8fc981e333f0fc80109c24c4a8b8
Razmik <razmik.minasyann@gmail.com>
2022-09-03 20:54:00 +0400

    add result

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -26,3 +26,8 @@ def check_win(player, computer):
       return "Scissors cuts paper! You win!"
     else:
       return "Rock smashes scissors! You lose"
+
+choices = get_choices()
+
+result = check_win(choices["player"], choices["computer"])
+print(result)
